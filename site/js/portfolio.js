222267e2d752779ed3b6603ba8be1566c4513a26
Adam Benyagoub <yazdraw@gmail..com>
2024-03-25 11:58:21 +0100

    Finished everything

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,10 +5,16 @@ function changeAvatar(){
 function changeName(){
     const color = prompt("Enter a color")
     const name = prompt("Enter your name")
+
+    //Editing the first name
     document.getElementById("firstname").innerText = name
     document.getElementById("firstname").style.color = "white"
+
+    //Editing the background color of the div
     document.getElementsByClassName("pink-bg")[0].style.backgroundColor = color;
 
+
+    // Getting all elements that are pink
     const elems = document.getElementsByClassName("pink-text")
 
     for(let i in elems){
