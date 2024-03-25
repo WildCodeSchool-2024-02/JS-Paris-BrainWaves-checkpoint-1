19a5bb03e11ad2ba82aeb780a7131da7b5dcb80a
Antoine <ant.delalande@gmail.com>
2024-03-25 11:38:13 +0100

    portfolio - js step 5 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,6 +10,7 @@ const nameButton = document.querySelector("#change-name")
 const firstName = document.querySelector("#firstname")
 const bgColor = document.querySelectorAll(".pink-bg")
 const textColor = document.querySelectorAll(".pink-text")
+const links = document.querySelectorAll("a")
 
 nameButton.addEventListener("click", () => {
     firstName.textContent = prompt("What's your name ?");
@@ -18,4 +19,6 @@ nameButton.addEventListener("click", () => {
         bg.style.backgroundColor = "#750ff7"
     for (let text of textColor)
         text.style.color = "#750ff7"
+    for (let link of links)
+        link.style.color = "#750ff7"
 })
