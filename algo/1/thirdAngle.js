cf917da5f61d28dec284748074c984417d08727e
BLACK-STAR <blackstar.pen.onetap@gmail.com>
2024-03-25 12:00:56 +0100

    First Commit

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -6,8 +6,22 @@ thirdAngle(90, 30) doit retourner 60
 thirdAngle(20, 80) doit retourner 80
 */
 
-function thirdAngle(a, b) {
+function ThirdAngle(a, b) {
   // Your code here !
+
+  if (typeof a !== 'number' || typeof b !== 'number') {
+      return "Les valeurs des angles doivent être des nombres.";
+  }
+  var thirdAngle = 180 - (a + b);
+
+  return thirdAngle;
 }
 
+let angleA = 60;
+let angleB = 45;
+let thirdAngle = ThirdAngle(angleA, angleB);
+console.log("Le troisième angle est : " + thirdAngle + " degrés.");
+
+
 module.exports = thirdAngle;
+
