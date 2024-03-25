4941085b70ea4adec8a6ac3ea580acc8201b35b1
Coline1186 <coline.grosso@neuf.fr>
2024-03-25 14:05:27 +0100

    finish JS step 2 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,4 +4,8 @@ changeLogo.addEventListener('click', function () {
   changeLogo.src = "image/avatar.svg";
 });
 
+const name = prompt("What is your name?");
+const element = document.getElementById("firstname");
+element.innerHTML = name;
+document.getElementById("firstname").style.color = "white";
 
