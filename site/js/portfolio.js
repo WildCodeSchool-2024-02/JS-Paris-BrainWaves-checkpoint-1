6dd2b69729e75de430c3e0e9919effdb49f991fa
Antoine <ant.delalande@gmail.com>
2024-03-25 11:26:04 +0100

    portfolio - js step 2 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,4 +5,12 @@ avatar.addEventListener("click", () => {
     avatar.src = "image/avatar-bis.png"
 })
 
+//Change name and color
+const nameButton = document.querySelector("#change-name")
+const firstName = document.querySelector("#firstname")
 
+nameButton.addEventListener("click", () => {
+    firstName.textContent = prompt("What's your name ?");
+    firstName.style.color = "white";
+
+})
