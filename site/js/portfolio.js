7d6a3624fe04cc174d2914b2a0266121fbe81018
Curtisdak <curtis.dakouri@gmail.com>
2024-03-25 14:28:39 +0100

    JS step 02  is Done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,16 @@ const avatar = document.querySelector(".avatar img");
 avatar.addEventListener("click", () => { 
    avatar.src = "image/avatar.svg";
 })
+
+const description = document.querySelector(".description");
+const firstName = document.querySelector("#firstname");
+
+const button = document.createElement("button");
+button.innerText= "Modify text and color";
+description.appendChild(button);
+
+button.addEventListener("click", () => {
+    let userName = prompt("write your name");
+    firstName.style.color="white";
+    firstName.innerHTML= userName;
+})
